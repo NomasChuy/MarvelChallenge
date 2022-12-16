42e0393d2ae050e543a0c8eafcd36daf64349056
Diego Garcia <diegogaral1996@outlook.com>
2022-12-16 12:40:12 -0500

    Removed parameters from fetch

--- app/src/main/java/com/example/avengerschallenge/data/repository/AvengersRepositoryImpl.kt
+++ app/src/main/java/com/example/avengerschallenge/data/repository/AvengersRepositoryImpl.kt
@@ -1,17 +1,17 @@
 package com.example.avengerschallenge.data.repository
 
-import com.example.avengerschallenge.data.AvengersAPI
+import com.example.avengerschallenge.data.api.AvengersAPI
 import com.example.avengerschallenge.data.mappers.toDomainMarvel
-import com.example.avengerschallenge.domain.AvengersRepository
+import com.example.avengerschallenge.domain.repository.AvengersRepository
 import com.example.avengerschallenge.domain.models.MarvelDomain
-import com.example.avengerschallenge.utils.Resource
+import com.example.avengerschallenge.core.utils.Resource
 import javax.inject.Inject
 
 class AvengersRepositoryImpl @Inject constructor(private val avengersAPI: AvengersAPI) :
-    AvengersRepository{
+    AvengersRepository {
     override suspend fun fetchAvengersComics(): Resource<List<MarvelDomain>> {
         return try {
-            val response = avengersAPI.fetchAvengers("","","")
+            val response = avengersAPI.fetchAvengers()
             if (response.isSuccessful){
                 val body = response.body()
                 val result = body?.avData?.results?.map {
